49f59cc9bf273b7a98a17e6dad86663781937f68
Bryan <bryansmith8023@gmail.com>
2022-02-01 15:25:40 -0600

    Week 3 Tuesday Afternoon

---
+++ COSC 3337 Data Science I/examples/linear_regression.py
@@ -0,0 +1,10 @@
+# imports
+import pandas as pd
+import seaborn as sns
+import statsmodels.formula.api as smf
+from sklearn.linear_model import LinearRegression
+from sklearn import metrics
+from sklearn.model_selection import train_test_split
+import numpy as np
+
+# allow plots to appear directly in the notebook
